c75afeb9879d35ad274deb59e2eb424f18a534ef
NarniaGuardian <al.smith823@gmail.com>
2020-06-16 07:53:56 -0700

    actually wait for cache to be set via async (#54)

--- src/spec_helper.spec.ts
+++ src/spec_helper.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing'
 import { CacheService } from '@app/service/cache.service'
+import { forkJoin } from 'rxjs'
 
 let cache: CacheService
 
@@ -7,9 +8,11 @@ const mockLeagues: any = require('doc/poe/api_trade_data_leagues.json')
 const mockStaticData: any = require('doc/poe/mockCurrenciesCache.json')
 const mockExchangeRates: any = require('doc/poe-ninja/currencyoverviewcache.json')
 
-beforeAll(() => {
+beforeAll((done) => {
   cache = TestBed.inject<CacheService>(CacheService)
-  cache.store(`leagues_1`, mockLeagues.result, 99999, false)
-  cache.store('currency_chaos_equivalents_Delirium', mockExchangeRates, 99999, false)
-  cache.store('currencies_1', mockStaticData, 99999, false)
+  forkJoin([
+    cache.store(`leagues_1`, mockLeagues.result, 99999, true),
+    cache.store('currency_chaos_equivalents_Delirium', mockExchangeRates, 99999, true),
+    cache.store('currencies_1', mockStaticData, 99999, true),
+  ]).subscribe(() => done())
 })
