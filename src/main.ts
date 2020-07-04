8e05ef6acb0293be1e59e19c7cf84ba89efd0c64
nomis51 <nomis51@users.noreply.github.com>
2020-07-04 19:19:13 -0400

    Toggle on/off mouse events for interactable parts of the overlay

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { environment } from '@env/environment'
 
+const setIgnoreMouseEvents = window.require('electron').remote.getCurrentWindow().setIgnoreMouseEvents
+
+/* Enable mouse events only if the mouse is over something it can interact with. Otherwise pass-through */
+window.addEventListener('mousemove', event => {
+  if (event.target === document.documentElement) {
+    setIgnoreMouseEvents(true, { forward: true });
+  } else {
+    setIgnoreMouseEvents(false)
+  }
+});
+
 if (environment.production) {
   enableProdMode()
 }
