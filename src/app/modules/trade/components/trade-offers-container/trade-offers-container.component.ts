b3ce043adbb18138d2de3bfcea09c5b443bb4124
nomis51 <nomis51@users.noreply.github.com>
2020-07-05 19:35:00 -0400

    Add comments

--- src/app/modules/trade/components/trade-offers-container/trade-offers-container.component.ts
+++ src/app/modules/trade/components/trade-offers-container/trade-offers-container.component.ts
@@ -19,6 +19,9 @@ import { CommandService } from '@modules/command/service/command.service'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnDestroy {
+  /**
+   * List of the current trade offers received and not ignored, removed or completed yet.
+   */
   public offers: Offer[] = []
 
   constructor(
@@ -32,10 +35,16 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     this.tradeService.tradeCancelled.subscribe(this.handleTradeCancelled.bind(this))
   }
 
+  /**
+   * Handles trade cancelled messages from chatListener
+   */
   private handleTradeCancelled(): void {
     this.resetOfferTrades()
   }
 
+  /**
+   * Handles trade accepted messages from chatListener
+   */
   private handleTradeAccepted(): void {
     const index = this.offers.findIndex((o) => o.tradeRequestSent)
 
@@ -57,6 +66,10 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     }
   }
 
+  /**
+   * Handles new offers coming from the chatListener
+   * @param offer New offer to add to the overlay
+   */
   private handleNewOffer(offer: Offer): void {
     if (offer) {
       this.offers.push(offer)
@@ -70,6 +83,10 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
 
   public ngOnDestroy(): void {}
 
+  /**
+   * Remove an offer from the list of trade offers
+   * @param offer Offer to remove
+   */
   public ignoreOffer(offer: Offer): void {
     const index: number = this.offers.findIndex(
       (o) =>
@@ -82,6 +99,9 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     }
   }
 
+  /**
+   * Remove the offer marked as Trade Accepted
+   */
   public removeAcceptedTradeOffer(): void {
     const index: number = this.offers.findIndex((o) => o.tradeRequestSent)
 
@@ -91,6 +111,9 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     }
   }
 
+  /**
+   * Reset the property tradeRequestSent to false on every offers
+   */
   public resetOfferTrades(): void {
     for (const o of this.offers) {
       o.tradeRequestSent = false
@@ -99,21 +122,40 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     this.cd.markForCheck()
   }
 
+  /**
+   * Remove the offer from the list of trade offers AND kick the buyer of the offer out of the party
+   * @param offer Offer to remove
+   */
   public removeOffer(offer: Offer): void {
     this.kickBuyer(offer.buyerName)
     this.ignoreOffer(offer)
   }
 
+  /**
+   * Kick a buyer out of the pary
+   * @param name Name of the buyer to kick
+   */
   public kickBuyer(name: string): void {
     this.commandService.command(`/kick ${name}`)
   }
 
+  /**
+   * Replace the special variables in the whisper to their value
+   * {item} => Item name
+   * {price} => Price of the item
+   * @param text Text of the whisper
+   * @param offer Offer related to the whisper
+   */
   private insertWhisperVars(text: string, offer: Offer): string {
     return text
       .replace('{item}', offer.itemName)
       .replace('{price}', `${offer.price.value} ${offer.price.currency}`)
   }
 
+  /**
+   * Send a "Thanks" whisper to the buyer
+   * @param offer Offer related to the whisper
+   */
   public sendThanksWhisper(offer: Offer): void {
     this.settingsService.get().subscribe((settings) => {
       const tradeSettings = settings as TradeUserSettings
@@ -128,6 +170,10 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     })
   }
 
+  /**
+   * Send a "Are you still interested in my Item X listed for Price Y?" to the buyer
+   * @param offer Offer related to the whisper
+   */
   public sendStillInterestedWhisper(offer: Offer): void {
     this.settingsService.get().subscribe((settings) => {
       const tradeSettings = settings as TradeUserSettings
@@ -142,6 +188,10 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     })
   }
 
+  /**
+   * Send a "I'm busy" whisper to the buyer
+   * @param offer Offer related to the whisper
+   */
   public sendBusyWhisper(offer: Offer): void {
     this.settingsService.get().subscribe((settings) => {
       const tradeSettings = settings as TradeUserSettings
@@ -156,6 +206,10 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     })
   }
 
+  /**
+   * Send a "Sold!" whisper to the buyer
+   * @param offer Offer related to the whisper
+   */
   public sendSoldWhisper(offer: Offer): void {
     this.settingsService.get().subscribe((settings) => {
       const tradeSettings = settings as TradeUserSettings
@@ -171,11 +225,19 @@ export class TradeOffersContainerComponent implements OnInit, AfterViewInit, OnD
     })
   }
 
+  /**
+   * Send a trade request command to the buyer
+   * @param offer Offer related to the trade request
+   */
   public sendTradeRequest(offer: Offer): void {
     this.commandService.command(`/tradewith ${offer.buyerName}`)
     offer.tradeRequestSent = true
   }
 
+  /**
+   * Send a party invite to the buyer
+   * @param offer Offer
+   */
   public sendPartyInvite(offer: Offer): void {
     this.commandService.command(`/invite ${offer.buyerName}`)
     offer.partyInviteSent = true
