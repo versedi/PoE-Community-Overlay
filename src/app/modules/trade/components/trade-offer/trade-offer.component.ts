041d5c0ff1c41ba04cf5a4a44dc1a7923bf9df79
nomis51 <nomis51@users.noreply.github.com>
2020-07-05 19:42:01 -0400

    Add comments

--- src/app/modules/trade/components/trade-offer/trade-offer.component.ts
+++ src/app/modules/trade/components/trade-offer/trade-offer.component.ts
@@ -20,8 +20,14 @@ import { TradeService } from '@modules/trade/services/trade.service'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
+  /**
+   * Offer to display
+   */
   @Input() public offer: Offer
 
+  /**
+   * Events related to chat commands and whispers
+   */
   @Output() public evIgnoreOffer = new EventEmitter()
   @Output() public evTradeRequest = new EventEmitter()
   @Output() public evPartyInvite = new EventEmitter()
@@ -31,6 +37,10 @@ export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
   @Output() public evRemoveOffer = new EventEmitter()
   @Output() public evPartyKick = new EventEmitter()
 
+  /**
+   * Floating buttons above the offer tile
+   * The buyer haven't been invited yet.
+   */
   public preInviteFloatingButtons: FloatingButton[] = [
     new FloatingButton(
       'Invite to party',
@@ -63,6 +73,10 @@ export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
     new FloatingButton('Ignore offer', 'red', 'delete', this.ignoreOffer.bind(this), () => null),
   ]
 
+  /**
+   * Floating buttons above the offer tile
+   * The buyer have been invited yet.
+   */
   public postInviteFloatingButtons: FloatingButton[] = [
     new FloatingButton(
       'Send trade request',
@@ -88,8 +102,9 @@ export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
     new FloatingButton('Remove offer', 'red', 'delete', this.removeOffer.bind(this), () => null),
   ]
 
-  public buyerInvited = false
-
+  /**
+   * Related to floating buttons show/hide process
+   */
   public showFloatingButtons = false
   public buttonTimeoutStarted = false
   public buttonTimeout: any = null
@@ -98,6 +113,10 @@ export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
     this.tradeService.buyer.subscribe(this.handleBuyerJoined.bind(this))
   }
 
+  /**
+   * Check if the name of player that has joined to area is the same as the buyer of the offer
+   * @param name Name of the player that has joined the area
+   */
   private handleBuyerJoined(name: string): void {
     if (this.offer && this.offer.buyerName === name) {
       this.offer.buyerJoined = true
@@ -105,46 +124,76 @@ export class TradeOfferComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void { }
 
-  public ngAfterViewInit(): void {}
+  public ngAfterViewInit(): void { }
 
-  public ngOnDestroy(): void {}
+  public ngOnDestroy(): void { }
 
+  /**
+   * Ask to send a trade request to the buyer
+   */
   public sendTradeRequest(): void {
     this.evTradeRequest.emit(this.offer)
   }
 
+  /**
+   * Ask to kick the buyer out of the party
+   */
   public kickBuyer(): void {
     this.evPartyKick.emit(this.offer)
   }
 
+  /**
+   * Ask to remove the offer from the list of offers available
+   * @param offer The offer to remove
+   */
   public removeOffer(offer: Offer): void {
     this.evRemoveOffer.emit(offer)
   }
 
+  /**
+   * Ask to send a party invite to the buyer
+   */
   public sendPartyInvite(): void {
     this.evPartyInvite.emit(this.offer)
     this.showFloatingButtons = false
     this.cd.markForCheck()
   }
 
+  /**
+   * Ask to send a "Sold" whisper to the buyer
+   */
   public sendSoldWhisper(): void {
     this.evSoldWhisper.emit(this.offer)
   }
 
+  /**
+   * Ask to send a "I'm busy" whisper to the buyer
+   */
   public sendBusyWhisper(): void {
     this.evBusyWhisper.emit(this.offer)
   }
 
+  /**
+   * Ask to ignore the offer
+   */
   public ignoreOffer(): void {
     this.evIgnoreOffer.emit(this.offer)
   }
 
+  /**
+   * Ask to send a "Are you still interested?" whisper to the buyer
+   */
   public sendStillInterestedWhisper(): void {
     this.evStillInterestedWhisper.emit(this.offer)
   }
 
+  /**
+   * Handles the show/hide process of floating buttons
+   * There's a delay while the floating buttons stay visible
+   * @param value Show / Hide the floating buttons
+   */
   public showFloatingButtonsContainer(value: boolean): void {
     if (value) {
       this.showFloatingButtons = true
